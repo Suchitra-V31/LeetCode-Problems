de1e1b6db884cc4aa9ecb44f6268ca4737446f25
Suchitra V <88223440+Suchitra-V31@users.noreply.github.com>
2024-03-22 17:36:17 +0530

    Time: 34 ms (99.76%) | Memory: 48.9 MB (27.72%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    var obj={};
+    return {
+        toBe : (num) => {
+            if(val===num){
+                return true;
+            }
+            else{
+              throw new Error("Not Equal")
+            }
+        
+        
+    },
+        notToBe : (num) =>{
+            if(val!==num){
+                return true;
+            } 
+            else{
+                throw new Error("Equal")
+            }
+    }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
