9ed7a1f81d6623541cdb528536cc759a7c0e2567
Suchitra V <88223440+Suchitra-V31@users.noreply.github.com>
2024-03-22 17:37:16 +0530

    Time: 50 ms (66.82%) | Memory: 49.1 MB (79.01%) - LeetSync

---
+++ 2864-is-object-empty/is-object-empty.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+     s = Object.keys(obj).length
+     if(s>0){
+          return false
+     }
+     else{
+          return true
+     }
+
+    
+};
